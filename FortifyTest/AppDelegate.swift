a194e9e6ba17c141bdec56388e336f9bb078d02a
Mark Goldman <mark.goldman@accenturefederal.com>
2018-01-30 13:21:59 -0500

    initialize WebRTC

--- FortifyTest/AppDelegate.swift
+++ FortifyTest/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let device = UIDevice.string(for: UIDevice.deviceType())
+        
+        print(device!)
+        print(RTCInitializeSSL())
+        
         return true
     }
 
